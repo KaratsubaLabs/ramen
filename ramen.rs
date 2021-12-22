e4d06685c2ae524c59d5d8ff0e3bb5d955cd2f76
Daniel Liu <mr.picklepinosaur@gmail.com>
2021-12-22 00:22:56 -0500

    simple arg parsing

--- ramen.rs
+++ ramen.rs
@@ -1,14 +1,50 @@
 
+static HELP_MSG: &str = "\
+USAGE:
+ramen [command]
+
+COMMANDS:
+build
+clean
+";
+
+fn help_and_exit() {
+    println!("{}", HELP_MSG);
+    std::process::exit(1);
+}
+
+fn build_command(args: &[String]) {
+    if args.len() < 2 {
+        help_and_exit()
+    }
+    let input = &args[0];
+    let output = &args[1];
+
+    println!("build command with {} {}", input, output);
+}
+
+fn clean_command() {
+    println!("clean command");
+}
+
+fn argparse(args: &[String]) {
+
+    let c = args.get(0);
+    if c.is_none() {
+        help_and_exit()
+    }
+
+    match &c.unwrap() as &str {
+        "build" => build_command(&args[1..]),
+        "clean" => clean_command(),
+        _ => help_and_exit(),
+    };
+
+}
+
 fn main() {
 
     let args: Vec<String> = std::env::args().skip(1).collect();
-    if args.len() != 2 {
-        println!("USAGE: ramen [input dir] [output dir]");
-        std::process::exit(1);
-    }
-
-    let _input = &args[0];
-    let _output = &args[1];
-
+    argparse(&args);
 }
 
