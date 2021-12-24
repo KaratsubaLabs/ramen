ae0c3d52774cd49d8639e530f2742eddab775554
Daniel Liu <mr.picklepinosaur@gmail.com>
2021-12-23 20:19:06 -0500

    builder pattern

--- filesystem.rs
+++ filesystem.rs
@@ -22,6 +22,46 @@ pub struct AnimeMeta {
     pub synopsis: String,
 }
 
+// TODO: make this better and less redundant
+#[derive(Debug)]
+struct AnimeMetaBuiilder {
+    pub title: Option<String>,
+    pub original_title: Option<String>,
+    pub synopsis: Option<String>,
+}
+
+impl AnimeMetaBuiilder {
+    pub fn new() -> AnimeMetaBuiilder {
+        AnimeMetaBuiilder {
+            title: None,
+            original_title: None,
+            synopsis: None
+        }
+    }
+
+    pub fn title(mut self, title: String) -> Self {
+        self.title = Some(title);
+        self
+    }
+    pub fn original_title(mut self, original_title: String) -> Self {
+        self.original_title = Some(original_title);
+        self
+    }
+    pub fn synopsis(mut self, synopsis: String) -> Self {
+        self.synopsis = Some(synopsis);
+        self
+    }
+
+    pub fn build(self) -> AnimeMeta {
+        // TODO handle the unwrap panics
+        AnimeMeta {
+            title: self.title.unwrap(),
+            original_title: self.original_title,
+            synopsis: self.synopsis.unwrap()
+        }
+    }
+}
+
 pub fn parse_all(root_dir: &str) -> io::Result<()> {
     
     let path = PathBuf::from(root_dir);
@@ -36,7 +76,7 @@ pub fn parse_all(root_dir: &str) -> io::Result<()> {
     Ok(())
 }
 
-pub fn parse_anime(anime_dir: &Path) -> io::Result<()> {
+fn parse_anime(anime_dir: &Path) -> io::Result<()> {
 
     // grab metadata
     let path = anime_dir.join("data");
@@ -57,16 +97,37 @@ pub fn parse_anime(anime_dir: &Path) -> io::Result<()> {
     Ok(())
 }
 
-pub fn parse_meta(meta_path: &Path) -> io::Result<()> {
+fn parse_meta(meta_path: &Path) -> io::Result<()> {
 
     let file = File::open(meta_path)?;
     let reader = io::BufReader::new(file);
 
+    let mut builder = AnimeMetaBuiilder::new();
+
     // TODO: reading lines from file could be more optimized
     for line in reader.lines() {
         let line = line?;
-        println!("{}", line);
+        let split = line.split_once("=");
+        if split.is_none() {
+            // TODO: possibly warn invalid config
+            continue;
+        }
+        let mut split = split.unwrap();
+        split.0 = split.0.trim();
+        split.1 = split.1.trim();
+
+        println!("{:?}", split);
+
+        match split.0 {
+            "title" => builder = builder.title(split.1.to_string()),
+            "original_title" => builder = builder.original_title(split.1.to_string()),
+            "synopsis" => builder = builder.synopsis(split.1.to_string()),
+            _ => ()
+        }
     }
+
+    let anime_data = builder.build();
+    println!("{:?}", anime_data);
 
     Ok(())
 }
