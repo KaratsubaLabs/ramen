49b2207ccca542d31bf20790348f4d5efa63fe91
Daniel Liu <mr.picklepinosaur@gmail.com>
2021-12-26 20:33:20 -0500

    anime info page mock

--- gen.rs
+++ gen.rs
@@ -7,7 +7,14 @@ use super::config;
 use super::config::{UserConfig};
 use super::parse::{AnimeData};
 
-pub fn generate_all(data: Vec<AnimeData>, user_config: &UserConfig) {
+pub fn generate_all(data: &Vec<AnimeData>, user_config: &UserConfig) {
 
 }
 
+fn generate_index_page() {
+
+}
+
+fn write_html(html: &str, indent_level: u8) {
+
+}
--- parse.rs
+++ parse.rs
@@ -43,7 +43,7 @@ pub struct AnimeMeta {
     pub synopsis: String,
     pub anime_type: AnimeType,
     // cover image
-    // release data
+    // release date
     // tags
 }
 
